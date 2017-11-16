4445f821d59fc7563a287c4191e2dd43751d8546
Seong ho Hong <grohong76@gmail.com>
2017-11-16 20:05:55 +0900

    UI setting

---
+++ TeamB/grohong/FacialAnalysis/FacialAnalysis/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FacialAnalysis
+//
+//  Created by Seong ho Hong on 2017. 11. 16..
+//  Copyright © 2017년 Seong ho Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var blurredImage: UIImageView!
+    @IBOutlet weak var selectImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
