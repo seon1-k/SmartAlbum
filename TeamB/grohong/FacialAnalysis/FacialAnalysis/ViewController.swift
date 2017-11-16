bb4de8f0ad3821baed01473ecc9658a97cd0b4f4
Seong ho Hong <grohong76@gmail.com>
2017-11-16 21:27:19 +0900

    pick picture

--- TeamB/grohong/FacialAnalysis/FacialAnalysis/ViewController.swift
+++ TeamB/grohong/FacialAnalysis/FacialAnalysis/ViewController.swift
@@ -9,20 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var selectedImage: UIImage? {
+        didSet {
+            self.blurredImage.image = selectedImage
+            self.selectImage.image = selectedImage
+        }
+    }
+    
     @IBOutlet weak var blurredImage: UIImageView!
     @IBOutlet weak var selectImage: UIImageView!
     
+    let picker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        picker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func galleryImagePickButton(_ sender: Any) {
+        picker.sourceType = .savedPhotosAlbum
+        picker.allowsEditing = true
+        self.present(picker, animated: true, completion: nil)
+    }
+    
 
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+       picker.dismiss(animated: true, completion: nil)
+        
+        guard let uiImage = info[UIImagePickerControllerEditedImage] as? UIImage else {
+            fatalError("unexpeted error pick picture")
+        }
+        self.selectedImage = uiImage
+    }
+}
