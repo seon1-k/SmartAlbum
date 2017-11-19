a232709289f664fc8feed7fe60274a6a881db79f
진형탁 <njir@naver.com>
2017-11-19 17:16:27 +0900

    feat: Setup project with CocoaPods

---
+++ TeamB/SmartAlbum/SmartAlbum/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SmartAlbum
+//
+//  Created by 진형탁 on 2017. 11. 19..
+//  Copyright © 2017년 team-b. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TeamB/SmartAlbum/SmartAlbum/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SmartAlbum
+//
+//  Created by 진형탁 on 2017. 11. 19..
+//  Copyright © 2017년 team-b. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
