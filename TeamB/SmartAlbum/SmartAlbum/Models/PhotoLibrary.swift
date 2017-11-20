38215bc13fbe47bb3bf67d6c73883cddbcf4a51c
진형탁 <njir@naver.com>
2017-11-20 22:09:37 +0900

    chore: Change image size for error

--- TeamB/SmartAlbum/SmartAlbum/Models/PhotoLibrary.swift
+++ TeamB/SmartAlbum/SmartAlbum/Models/PhotoLibrary.swift
@@ -20,20 +20,20 @@ class PhotoLibrary {
         requestOptions = PHImageRequestOptions()
         requestOptions.isSynchronous = true
         fetchOptions = PHFetchOptions()
-        fetchOptions.predicate = NSPredicate(format: "mediaType == %d || mediaType == %d",
-                                                 PHAssetMediaType.image.rawValue,
-                                                 PHAssetMediaType.video.rawValue)
         fetchOptions.sortDescriptors = [NSSortDescriptor(key:"creationDate", ascending: false)]
-        fetchResult = PHAsset.fetchAssets(with: fetchOptions)
+        fetchResult = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: fetchOptions)
     }
-    
+
     var count: Int {
         return fetchResult.count
     }
     
     func setPhoto(at index: Int, completion block: @escaping (UIImage?)->()) {
         if index < fetchResult.count  {
-            imgManager.requestImage(for: fetchResult.object(at: index) as PHAsset, targetSize: UIScreen.main.bounds.size, contentMode: PHImageContentMode.aspectFill, options: requestOptions) { (image, _) in
+            // As the size increases, there is an error that the image in the collectionview is not shown.
+            // let thumbnailSize = UIScreen.main.bounds.size
+            let thumbnailSize = CGSize(width: 100, height: 100)
+            imgManager.requestImage(for: fetchResult.object(at: index) as PHAsset, targetSize: thumbnailSize, contentMode: PHImageContentMode.aspectFill, options: requestOptions) { (image, _) in
                 block(image)
             }
         } else {
